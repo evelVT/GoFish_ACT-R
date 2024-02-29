e9e283a0fbb4d3e33da8e87d2f3f4c6231b3090f
EvelVT <e.van.tricht@ziggo.nl>
2024-02-29 15:02:54 +0100

    Initial Commit

---
+++ GoFish_ACT-R/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoFish_ACT-R
+//
+//  Created by Evelien van Tricht on 29/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoFish_ACT-R/GoFish_ACT_RApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoFish_ACT_RApp.swift
+//  GoFish_ACT-R
+//
+//  Created by Evelien van Tricht on 29/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct GoFish_ACT_RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
