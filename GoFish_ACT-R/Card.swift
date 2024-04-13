e89e82dd59f8b1b8f1af3a970c629365d1aab7de
Mihai <m.g.tanase@student.rug.nl>
2024-04-13 17:04:56 +0200

    Removed Rank and Suit enum from Card

--- GoFish_ACT-R/Card.swift
+++ GoFish_ACT-R/Card.swift
@@ -1,109 +1,74 @@
 import SwiftUI
-//Player class
 
-class Player: ObservableObject, Identifiable {
-    let id: Int
-    @Published var name: String
-    @Published var score = 0
-    private(set) var hand: [Card] = []
-    @Published var gfModel: GFModel?
+enum Suit: String, CaseIterable {
+    case hearts = "Hearts"
+    case diamonds = "Diamonds"
+    case clubs = "Clubs"
+    case spades = "Spades"
+}
+enum Rank: Int, CaseIterable, CustomStringConvertible {
+    // Represents the initial state where the rank is unknown
+    case two = 2, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king, ace
 
-    init(id: Int, name: String) {
-        self.id = id
-        self.name = name
-        if id != 1  {
-            self.gfModel = GFModel(id: id)
+    static func from(string: String) -> Rank {
+           switch string.lowercased() {
+           case "two": return .two
+           case "three": return .three
+           case "four": return .four
+           case "five": return .five
+           case "six": return .six
+           case "seven": return .seven
+           case "eight": return .eight
+           case "nine": return .nine
+           case "ten": return .ten
+           case "jack": return .jack
+           case "queen": return .queen
+           case "king": return .king
+           case "ace": return .ace
+           default: return .ace
+           }
+       }
 
-        }
 
-    }
-    func emptyHand() {
-        hand.removeAll()
-    }
-
-    func addCardPlayer(card: Card) {
-        print("\(name) receives card!")
-        hand.append(card)
-        sortHand()
-        objectWillChange.send()
-        print("\(name) now has \(hand.count) cards!")
-        print("Check for books in \(name)'s hand!")
-        score += checkForBooks()
-    }
-
-    func addCard(card: Card) {
-        print("\(name) receives card!")
-        hand.append(card)
-        sortHand()
-        objectWillChange.send()
-        print("\(name) now has \(hand.count) cards!")
-    }
-
-    func makeSets() {
-        print("Check for books in \(name)'s hand!")
-        score += checkForBooks()
-    }
-
-    func giveNoCards(ofRank rank: Rank) -> Int {
-        let matchingCards = hand.filter { $0.rank == rank }
-        let num = matchingCards.count
-        return num
-    }
-
-    func removeCard(card: Card) {
-        hand.removeAll(where: {$0 == card})
-    }
-
-    // Function that will be called when the player is asked for a specific rank
-    // Returns all cards of the specified rank and removes them from the player's hand
-    func giveAllCards(ofRank rank: Rank) -> [Card] {
-        let matchingCards = hand.filter { $0.rank == rank }
-        hand = hand.filter { $0.rank != rank }
-        return matchingCards
-    }
-
-    // Check if the player has a card of the specified rank
-    //$0 some weird shorthand for basically saying "for each card in hand, check if the rank is equal to the rank passed in"
-    func hasCard(ofRank rank: Rank) -> Bool {
-        return hand.contains { $0.rank == rank }
-    }
-
-    // Choose a rank to ask for from another player
-    func chooseCardToAskFor() -> Rank? {
-        // TODO: Implement logic to send the cards to the player who asked for them
-       return nil
-    }
-
-    func sortHand() {
-        hand = hand.sorted { (lhs, rhs) in
-            if lhs.rank == rhs.rank { // <1>
-                return lhs.suit.rawValue > rhs.suit.rawValue
-            }
-
-            return lhs.rank.rawValue > rhs.rank.rawValue // <2>
+    var description: String {
+        switch self {
+        case .two: return "two"
+        case .three: return "three"
+        case .four: return "four"
+        case .five: return "five"
+        case .six: return "six"
+        case .seven: return "seven"
+        case .eight: return "eight"
+        case .nine: return "nine"
+        case .ten: return "ten"
+        case .jack: return "jack"
+        case .queen: return "queen"
+        case .king: return "king"
+        case .ace: return "ace"
         }
     }
+}
 
-    // Method to check for books and remove them from the player's hand
-    // Returns the number of books found and removed
-    //not sure when this would be called. Each time the player receives a card? So in receiveCard?
-    func checkForBooks() -> Int {
-        var booksCount = 0
-        let ranks = hand.map { $0.rank }
-        let uniqueRanks = Set(ranks)
 
-        for rank in uniqueRanks {
-            let count = ranks.filter { $0 == rank }.count
-            if count == 4 {
-                // Found a book
-                booksCount += 1
-                hand.removeAll { $0.rank == rank }
-                // This then should be showed in the view
-            }
-        }
+//Card struct.
 
-        return booksCount
+//Equatable: A type that can be compared for value equality.
+struct Card: Equatable {
+
+    let suit: Suit
+    let rank: Rank
+    var open: Bool
+    var drag: Bool
+
+    mutating func toggleOpen() {
+        open.toggle()
+    }
+    mutating func toggleDrag() {
+        drag.toggle()
     }
 
-
+    static func == (lhs: Card, rhs: Card) -> Bool {
+        return lhs.suit == rhs.suit && lhs.rank == rhs.rank
+    }
 }
